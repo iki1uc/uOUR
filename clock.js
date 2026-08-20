dcf2f759f2b5d8f8a341df74c4cbc43b168a6acf
iki1uc <entertrainer@web.de>
2026-08-21 00:18:06 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -11,3 +11,23 @@ setInterval(() => {
   document.getElementById("clock-work").innerText =
     new Date(workSeconds * 1000).toISOString().substr(11, 8);
 }, 1000);
+
+const Axiom3D = {
+  h: 0,   // Höhe
+  b: 0,   // Breite
+  t: 0    // Tiefe
+};
+
+let h = 0; // Höhe
+let b = 0; // Breite
+let t = 0; // Tiefe
+
+setInterval(() => h++, 1000);
+setInterval(() => b++, 1000);
+setInterval(() => t++, 1000);
+
+const Matrix3x3 = [
+  [h, b, t],
+  [h, b, t],
+  [h, b, t]
+];
