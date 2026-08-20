1206f0118ef119dc480537e73b49e50c51e3ed00
iki1uc <entertrainer@web.de>
2026-08-21 01:47:15 +0200

    Create A.js

---
+++ A.js
@@ -0,0 +1,54 @@
+// A.js – Master-Modul für uOUR/OURu
+
+import { Axiom698869 } from "./698869.js";
+import { HighwayAxiom } from "./Highway-Axiom.js";
+import { KomplettumAxiom } from "./Komplettum-Axiom.js";
+import { MEAxiom } from "./ME-Axiom.js";
+import { OperatorAxiom } from "./Operator-Axiom.js";
+import { RADAxiom } from "./RAD-Axiom.js";
+import { ScanAxiom } from "./Scan-Axiom.js";
+import { SlalomAxiom } from "./Slalom-Axiom.js";
+import { PhiAxiom } from "./Phi-Axiom.js";
+import { IORAxiom } from "./IOR-Axiom.js";
+import { FlagAxiom } from "./flag.vector.js";
+
+export const A = {
+
+  NAME: "A",
+  TICK: 0,
+  VALUE: 0,
+  RADIUS: 0,
+  ORBIT: 0,
+
+  axioms() {
+    return {
+      ax698869: Axiom698869,
+      highway: HighwayAxiom,
+      komplettum: KomplettumAxiom,
+      me: MEAxiom,
+      operator: OperatorAxiom,
+      rad: RADAxiom,
+      scan: ScanAxiom,
+      slalom: SlalomAxiom,
+      phi: PhiAxiom,
+      ior: IORAxiom,
+      flag: FlagAxiom
+    };
+  },
+
+  calc() {
+    this.TICK++;
+    this.VALUE++;
+
+    // Orbit-Berechnung (TMP-kompatibel)
+    this.RADIUS = Math.sqrt(this.VALUE * this.VALUE + this.TICK);
+    this.ORBIT = Math.sin(this.TICK / 10) * this.RADIUS;
+  },
+
+  run() {
+    this.calc();
+    console.log("A:", this.VALUE, this.RADIUS, this.ORBIT, this.TICK);
+  }
+};
+
+setInterval(() => A.run(), 1000);
